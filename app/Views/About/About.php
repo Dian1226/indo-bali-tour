3be78f2e2d5019cc63d579e85e927c069bcf2af7
ayumas <jaeminpacaqu@gmail.com>
2024-02-21 14:58:52 +0800

    about sudah selesai

---
+++ app/Views/About/About.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>About Indo Bali Tour</title>
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <link rel="stylesheet" type="text/css" href="/public/About/CSS/About.css">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600&family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
+
+        <!--========== Boxicoins Link ===========-->
+        <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    </head>
+
+        <!-- HEADER -->
+        <header>
+            <div class="background">
+                <img src="/public/About/About/About/pura_ulun_danu_danau_1rGrw-transformed (1).jpeg" alt="">
+                <div class="heading">
+                    <h1>Apa Yang <br>Anda Cari?</h1>
+                    <p>Baca Blog Harian Kami!</p>
+                </div>
+            </div>
+        </header>
+
+    <body>
+        <nav>
+            <div class="navbar">
+                <div class="logo"><a href="#">ABOUT US</a></div>
+            </div>
+        </nav>
+        <div>
+            <h1 class="desk1">
+                ABOUT US
+            </h1>
+            <hr class="h">
+    </div>
+    
+        <script src="/public/About/JS/about.js"></script>
+    </body>
+</head>
+
+        <!--========================ABOUT=============================-->
+<body>
+        <div class="row">
+        <div class="left">
+            <img src="/public/About/About/About/GWK Culture Park_Kiri.jpg">
+        </div>
+        <div class="right">
+            <div class="content">
+                <h3>Welcome to Indo Bali Tour, Your Quality Travel Partner</h3>
+                <p>Indo Bali Tour is a leading tour and tourism company that specialize in creating unforgettable experiences of travelers exploring the enchanting destinations of Indonesia and Bali.
+
+                    Established in 2014, we have emerged as a trusted name in the industry, known for our commitment to delivering exceptional service, unique itineraries, and a deep understanding of the diverse cultures and landscapes of this remarkable region. 
+                    
+                    We will bring you a great memory of our togetherness exploring the island. For this, we will choose the most joyful tour packages for you. Or, you plan your own tour and we make it memorable moment.
+                    
+                    Let's customize the tour together and be flexibly decide it during the trip. This makes IBT unique an be your quality travel partner. 
+                    
+                    Looking forward to having quality travel and joyful days with you!</p>
+            </div>
+        </div>
+       </div>
+
+       <div class="row">
+        <div class="left left3">
+            <img src="/public/About/About/About/VISION.jpg">
+        </div>
+        <div class="right right3">
+            <div class="content">
+                <h3>OUR VISION</h3>
+                <p>We envision Indo Bali Tour as the ultimate gateway to Indonesia and Bali, 
+                    making these destinations accessible and enjoyable for travelers from around the world. Our vision is to be recognized as a leader in sustainable tourism, 
+                    contributing positively to the local communities and environments we serve.</p>
+            </div>
+        </div>
+       </div>
+
+       <div class="row">
+        <div class="right right2">
+            <img src="/public/About/About/About/MISSION.jpg">
+        </div>
+        <div class="left left2">
+            <div class="content">
+                <h3>OUR MISIION</h3>
+                <p>Our mission at Indo Bali Tour is to craft immersive and authentic travel experiences that connect our clients with the rich cultural heritage, 
+                    stunning natural beauty, and vibrant traditions of Indonesia and Bali. 
+                    We aim to exceed expectations by providing personalized, safe, and memorable journeys.</p>
+            </div>
+        </div>
+       </div>
+
+       <div class="row">
+        <div class="left left3">
+            <img src="/public/About/About/About/WHY CHOOSE US.jpg">
+        </div>
+        <div class="right right3">
+            <div class="content">
+                <h3>WHY CHOOSE US?</h3>
+                <p><strong>Local Expertise:</strong> Our team comprises seasoned experts who are passionate about Bali. We possess in-depth knowledge of the destinations, ensuring a rich and insightful travel experience.
+
+                    <br><strong>Service:</strong> We understand that every traveler is unique, and we tailor our services to meet individual preferences and requirement.
+                    
+                    <br><strong>Sustainability:</strong> We committed to responsible tourism, striving to minimize our environmental impact and support local communities.
+                    
+                    <br><strong>Safety:</strong> Your safety is our top priority. We adhere to the highest safety standards, ensuring worry-free travel.
+                    
+                    <br><strong>Value:</strong> We offer competitive pricing without compromising on the quality of our services.</p>
+            </div>
+        </div>
+       </div>
+
+
+               <!--======= Footer section design ========-->
+               <footer class="Footer">
+                <div class="Footer-content">
+                    <img src="/public/footer/img/Logo_IBT.png">
+    
+                    <div class="icons">
+                        <br><h4>Folow Us</h4>
+                        <a href="tel:+6282144678456"><i class='bx bxs-phone'></i></a>
+                        <a href="https://wa.me/6282144678456"><i class='bx bxl-whatsapp-square'></i></a>
+                        <a href="mailto:enjoy@indobalitour.com"><i class='bx bxs-envelope'></i></a>
+                        <a href="https://www.youtube.com/@indobalitour2505"><i class='bx bxl-youtube'></i></a>
+                        <a href="#https://www.facebook.com/indobalitour"><i class='bx bxl-facebook-circle'></i></a>
+                        <a href="#https://www.instagram.com/indobalitourcom/"><i class='bx bxl-instagram-alt'></i></a>
+    
+                        <div class="Footer-content payment-section">
+                            <br><h4>Payment</h4>
+                            <img src="/public/footer/img/Logo payment BRI_IBT (1).jpg" width="100" height="70" alt="Payment Logo">
+                        </div></br>
+                    </div></br>
+                </div>
+    
+                <div class="Footer-content">
+                    <br><h4>About IBT</h4>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Tours</a></li>
+                        <li><a href="#">Blog & Events</a></li>
+                        <li><a href="#">Member</a></li>
+                        <li><a href="#">Tour Experience</a></li>
+                        <li><a href="#">Packages</a></li>
+                        <li><a href="#">Contact Us</a></li><br>
+                    </ul>
+    
+                    <div class="Footer-content">
+                        <br><strong><h4>Excellent Review</h4></strong>
+                        <img src="/public/footer/img/Tripadvisor.jpg" alt="Tripadvisor Logo"></br>
+                    </div>
+                </div>
+    
+                <div class="Footer-content">
+                    <br><h4>IBT Recommended Tour</h4>
+                    <ul>
+                        <li><a href="#">Promo</a></li>
+                        <li><a href="#">Adventures</a></li>
+                        <li><a href="#">Fun Activities</a></li>
+                        <li><a href="#">Transport Service</a></li>
+                        <li><a href="#">Airport Transfer</a></li><br>
+    
+                        <div class="Footer-content G-maps">
+                            <h4><br>Our Location</h4> 
+                            <h5>Jln. Ganetri IV No. 4 Denpasar 80237 Bali</h5>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3944.553840585376!2d115.22995007450947!3d-8.638757491407803!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd23f79d0b9c291%3A0x28fcef04fc4ddfb!2sJl.%20Ganetri%20IV%20No.4%2C%20Tonja%2C%20Kec.%20Denpasar%20Utara%2C%20Kota%20Denpasar%2C%20Bali%2080235!5e0!3m2!1sid!2sid!4v1707716498050!5m2!1sid!2sid" width="350" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+                    </ul>
+                </div>
+    
+                <div class="Footer-Bottom">
+                    <p>Copyright &copy;2024; Designed by <span class="designer">INDO APPS SOLUSINDO</span></p>
+                </div>
+                
+                    
+            </footer>
+        </body>
+</html>
+    
