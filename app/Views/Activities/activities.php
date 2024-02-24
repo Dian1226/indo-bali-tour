f55fc3f8e600256630765ad689465d7dfbd2b460
unknown <dwiari.>
2024-02-24 08:21:50 +0800

    sip

---
+++ app/Views/Activities/activities.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FUN ACTIVITIES</title>
+
+    <!-- ---------icon----- -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <!-- --------AOS------ -->
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
+    <!-- -------CSS------- -->
+    <link rel="stylesheet" href="activities.css">
+</head>
+<body>
+    <nav>
+        <div class="navbar">
+            <i class='bx bx-menu'></i>
+            <div class="logo"><a href="#">TOURS</a></div>
+            <div class="nav-links">
+                <div class="sidebar-logo">
+                    <span class="logo-name">TOURS</span>
+                    <i class='bx bx-x '></i>
+                </div>
+                <ul class="links">
+                    <li>
+                        <a href="#">RECOMENDATION</a>
+                        <i class='bx bxs-chevron-down arrow html-arrow'></i>
+                        <ul class="html-submenu sub-menu">
+                            <li><a href="#">Adventures</a></li>
+                            <li><a href="#">Fun Activities</a></li>
+                            <li><a href="#">Transport Service</a></li>
+                            <li><a href="#">Airport Service</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">TRENDING</a>
+                        <i class='bx bxs-chevron-down arrow js-arrow'></i>
+                        <ul class="js-submenu sub-menu">
+                            <li><a href="#">Destinasion</a></li>
+                            <li><a href="#">Package</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">PROMO</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container-adv">
+        <img src="image projek PKL/goa-gajah-bali[1].jpg">
+        <h1 class="desk-adv">Fun Activities</h1>
+        <ul class="adv-links">
+            <li class="link-adv"><a href="#">home ></a></li>
+            <li class="link-adv"><a href="#">category ></a></li>
+            <li class="link-adv"><a href="#">activities ></a></li>
+        </ul>
+    </div>
+
+<!-- ============================================================================================================================== -->
+
+    <div id="card-area">
+        <div class="wrapper">
+            <div class="box-area">
+                <div class="box">
+                    <img src="image projek PKL/snorkeling.jpg" alt="">
+                    <div class="overlay">
+                        <h3>snorkeling</h3>
+                        <a href="#">View Detail</a>
+                    </div>
+                </div>
+
+                <div class="box">
+                    <img src="image projek PKL/tubing adventures.jpg" alt="">
+                    <div class="overlay">
+                        <h3>tubing adventures</h3>
+                        <a href="#">View Detail</a>
+                    </div>
+                </div>
+
+                <div class="box">
+                    <img src="image projek PKL/jeep tour.jpg" alt="">
+                    <div class="overlay">
+                        <h3>jeep tour</h3>
+                        <a href="#">View Detail</a>
+                    </div>
+                </div>
+
+                <div class="box">
+                    <img src="image projek PKL/camel riding.jpg" alt="">
+                    <div class="overlay">
+                        <h3>camel riding</h3>
+                        <a href="#">View Detail</a>
+                    </div>
+                </div>
+                
+                <div class="box">
+                    <img src="image projek PKL/bali hai reef cruise.jpg" alt="">
+                    <div class="overlay">
+                        <h3>reef cruise</h3>
+                        <a href="#">View Detail</a>
+                    </div>
+                </div>
+
+                <div class="box">
+                    <img src="image projek PKL/bali riding horse.jpg" alt="">
+                    <div class="overlay">
+                        <h3>horse riding</h3>
+                        <a href="#">View Detail</a>
+                    </div>
+                </div>
+
+                <div class="box">
+                    <img src="image projek PKL/swim with dolphin.jpg" alt="">
+                    <div class="overlay">
+                        <h3>swim with dolphin</h3>
+                        <a href="#">View Detail</a>
+                    </div>
+                </div>
+
+                <div class="box">
+                    <img src="image projek PKL/water sport.jpg" alt="">
+                    <div class="overlay">
+                        <h3>water sport</h3>
+                        <a href="#">View Detail</a>
+                    </div>
+                </div>
+
+                <div class="box">
+                    <img src="image projek PKL/tracking sunrise.jpg" alt="">
+                    <div class="overlay">
+                        <h3>tracking sunrice</h3>
+                        <a href="#">View Detail</a>
+                    </div>
+                </div>
+
+                <div class="box">
+                    <img src="image projek PKL/romantic sunset dinner.jpg" alt="">
+                    <div class="overlay">
+                        <h3>sunset dinner</h3>
+                        <a href="#">View Detail</a>
+                    </div>
+                </div>
+
+                <div class="box">
+                    <img src="image projek PKL/elephant ride.jpg" alt="">
+                    <div class="overlay">
+                        <h3>elephant ride</h3>
+                        <a href="#">View Detail</a>
+                    </div>
+                </div>
+
+                <div class="box">
+                    <img src="image projek PKL/bali safari.jpg" alt="">
+                    <div class="overlay">
+                        <h3>bali safari park</h3>
+                        <a href="#">View Detail</a>
+                    </div>
+                </div>
+
+                <div class="box">
+                    <img src="image projek PKL/zoo park.jpg" alt="">
+                    <div class="overlay">
+                        <h3>bali zoo park</h3>
+                        <a href="#">View Detail</a>
+                    </div>
+                </div>
+
+                <div class="box">
+                    <img src="image projek PKL/bali sea walker.jpg" alt="">
+                    <div class="overlay">
+                        <h3>bali sea walker</h3>
+                        <a href="#">View Detail</a>
+                    </div>
+                </div>
+
+                <div class="box">
+                    <img src="image projek PKL/scuba diving.jpg" alt="">
+                    <div class="overlay">
+                        <h3>bali scuba diving</h3>
+                        <a href="#">View Detail</a>
+                    </div>
+                </div>
+
+                <div class="box">
+                    <img src="image projek PKL/bicycle.jpg" alt="">
+                    <div class="overlay">
+                        <h3>bicycle</h3>
+                        <a href="#">View Detail</a>
+                    </div>
+                </div>
+
+                <div class="box">
+                    <img src="image projek PKL/atv.jpg" alt="">
+                    <div class="overlay">
+                        <h3>ATV</h3>
+                        <a href="#">View Detail</a>
+                    </div>
+                </div>
+
+                <div class="box">
+                    <img src="image projek PKL/water rafting.jpg" alt="">
+                    <div class="overlay">
+                        <h3>water rafting</h3>
+                        <a href="#">View Detail</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+    
+    <script src="activities.js"></script>
+</body>
+</html>
